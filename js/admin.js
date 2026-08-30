0f497a00a9364bc9b6ee8823e241046d2a5025f9
SinuraManudhaya-svg <manudhayasinura@gmail.com>
2026-08-30 16:30:48 +0530

    Refactor admin.js to improve structure and security

--- js/admin.js
+++ js/admin.js
@@ -15,7 +15,7 @@
 'use strict';
 
 const $ = id => document.getElementById(id);
-const API_BASE = 'http://localhost:3000/api';
+const API_BASE = '/api';
 const TOKEN_KEY = 'edm_admin_token';
 const PASSWORD_KEY = 'edm_admin_password';
 
